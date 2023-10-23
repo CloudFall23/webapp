2544b3a4693bbb975d276c62a3589a3e171fa405
GargavaSiddharthNEU <114346515+GargavaSiddharthNEU@users.noreply.github.com>
2023-10-22 20:50:54 -0400

    ssl changes

--- util/database.js
+++ util/database.js
@@ -3,16 +3,41 @@ const Sequelize = require('sequelize');
 const dbvar = require('./dbvar');
 //console.log(process.env.DB_USER);
 
-//DB SPECs
-const sequelize = new Sequelize(process.env.DB_POSTGRESQL, process.env.DB_USER, process.env.DB_PASSWORD, {
+const isAwsRDS = process.env.DB_HOST && process.env.DB_HOST.includes('localhost');
+
+const sequelizeConfig = {
   dialect: 'postgres',
   host: process.env.DB_HOST,
-  // dialectOptions: {
-  //   ssl: {
-  //       require: true,
-  //       rejectUnauthorized: false
-  //   }
-  // },
-});
+};
+
+if (!isAwsRDS) {
+  sequelizeConfig.dialectOptions = {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  };
+}
+
+const sequelize = new Sequelize(process.env.DB_POSTGRESQL, process.env.DB_USER, process.env.DB_PASSWORD, sequelizeConfig);
+
+//Old Stuff ---
+//DB SPECs
+// const sequelize = new Sequelize(process.env.DB_POSTGRESQL, process.env.DB_USER, process.env.DB_PASSWORD, {
+//   dialect: 'postgres',
+//   host: process.env.DB_HOST,
+//   // dialectOptions: {
+//   //   ssl: {
+//   //       //require: process.env.DB_HOST == 'localhost'?false:true,
+//   //       require: false,
+//   //       rejectUnauthorized: false
+//   //   }
+//   // },
+
+
+// });
+//Old stuff ---
+
+
 
 module.exports = sequelize;
