682891e44d92c289605a07921cae8885a6cccfcf
GargavaSiddharthNEU <114346515+GargavaSiddharthNEU@users.noreply.github.com>
2023-10-24 18:50:55 -0400

    New user access

--- setup.sh
+++ setup.sh
@@ -40,6 +40,36 @@ echo "unzipped successfully"
 
 npm install 
 
+# Create a non-admin user
+sudo useradd -m webappuser
+sudo groupadd webappgroup
+ 
+# Add webappuser and admin to the webappgroup
+sudo usermod -aG webappgroup webappuser
+sudo usermod -aG webappgroup admin
+ 
+# Set ownership and permissions for webappuser's home directory
+sudo chown -R webappuser:webappgroup /home/webappuser
+sudo chmod -R 750 /home/webappuser
+ 
+# Set ownership and permissions for the app.js file in admin's directory
+sudo chown webappuser:webappgroup /home/admin/webApp/server.js
+sudo chmod 750 /home/admin/webApp/server.js
+ 
+# Add webappuser to the systemd-journal group
+sudo usermod -aG systemd-journal webappuser
+ 
+# Set the .env file in admin's directory
+sudo chmod 644 /home/admin/webApp/.env
+ 
+# Create the log file and set ownership and permissions
+sudo touch /var/log/webapp.log
+sudo chown webappuser:webappgroup /var/log/webapp.log
+sudo chmod 644 /var/log/webapp.log
+ 
+# Set the .env file in admin's directory
+sudo chmod 600 /home/admin/webApp/.env
+
 echo "shell file successfully"
 
 sudo mv /tmp/webApp.service /etc/systemd/system/webApp.service
